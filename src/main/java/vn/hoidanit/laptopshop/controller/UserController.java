b6a1cbd7ac998b0a1162b28a6b4fd867dd70cd7d
Tinab <trandinhthien0104@gmail.com>
2024-10-27 21:46:36 +0700

    Using PathVariable annotation, create user detail interface

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import vn.hoidanit.laptopshop.domain.User;
@@ -50,6 +51,14 @@ public class UserController {
     public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
+    }
+
+    @RequestMapping("/admin/user/{id}") //Không khai báo method thì mặc định là GET
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        System.out.println("check path id = " + id);
+        model.addAttribute("id", id);
+        model.addAttribute("newUser", new User());
+        return "admin/user/show";
     }
 
     @RequestMapping(value = "/admin/user/create", method=RequestMethod.POST)
