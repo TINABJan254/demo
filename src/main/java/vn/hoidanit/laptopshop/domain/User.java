15aa9c5b25e57c40eb58d1fabe696475f65ab4a9
Tinab <trandinhthien0104@gmail.com>
2024-11-01 21:41:09 +0700

    Define many-to-one relationships

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,11 +1,18 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,7 +20,13 @@ public class User {
 
     private String email, password, fullName, address, phone, avatar;
 
-    //roleId
+    //many - one <=> users - role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
 
     public User() {
 
