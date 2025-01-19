0563dc390cd6823358e3998f3498d52f8c8a5a12
Tinab <trandinhthien0104@gmail.com>
2025-01-19 16:04:28 +0700

    Enable spring security, custom class UserDetailsService

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -10,7 +10,9 @@ import vn.hoidanit.laptopshop.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
-    List<User> findByEmail(String email);
+    List<User> findOneByEmail(String email);
     User findById(long id);
     boolean existsByEmail(String email);
+
+    User findByEmail(String email);
 }
